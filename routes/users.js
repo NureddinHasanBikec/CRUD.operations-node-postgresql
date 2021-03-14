272d5fe967468003e46687b598be42923f89e258
NureddinHasanBikec <nureddinhasanbikec@gmail.com>
2021-03-14 18:40:04 +0300

    Added controllers

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+const usersControllers = require("../controllers/usersController");
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', usersControllers.get_users);
 
 module.exports = router;
